698e0db89dabebea4533384c803db6e8c62469db
CHZS1 <109904048+CHZS1@users.noreply.github.com>
2023-04-14 01:06:29 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,3 +10,30 @@ rangeDataOne.oninput = function getDigitOne() {
 rangeDataTwo.oninput = function getDigitTwo() {
     digitTwo.innerHTML = this.value;
 }
+
+//sent to calc data
+let sentToCalc = document.getElementById('sent-to-calc');
+let boxSelect = document.getElementById('box-select').value;
+let typeOfBox = document.getElementById('type-of-box');
+
+let distanceSelect = document.getElementById('dist-select').value;
+let distance = document.getElementById('distance');
+
+let timeOfRent = document.getElementById('time-of-rent');
+let quantity = document.getElementById('quantity-of-boxes');
+
+function sentAll() {
+    let form = document.querySelector('.sent-to-serv__wrap');
+    form.classList.add('active');
+
+    typeOfBox.value = boxSelect;
+    distance.value = distanceSelect;
+    timeOfRent.value = rangeDataOne.value;
+    quantity.value = rangeDataTwo.value;
+};
+
+
+function closeForm() {
+    let active = document.querySelector('.active');
+    active.classList.remove('active');
+}
